2351a33efa69663ae375082dbf81f1832bcf7e36
Chris Tang <1073412@hr.nl>
2025-06-22 17:47:48 +0200

    ADD Nav-bar

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import ListScreen from './screens/list';
+import StationDetailScreen from './screens/stationDetail';
+import MapScreen from './screens/map';
+import SettingScreen from './screens/setting';
+import { DataProvider } from './components/data';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function ListStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="List" component={ListScreen} options={{ title: 'List' }} />
+            <Stack.Screen name="Station" component={StationDetailScreen} options={{ title: 'Details' }} />
+        </Stack.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <DataProvider>
+            <NavigationContainer>
+                <Tab.Navigator initialRouteName="List">
+                    <Tab.Screen name="List" component={ListStack} options={{ headerShown: false }} />
+                    <Tab.Screen name="Map" component={MapScreen} />
+                    <Tab.Screen name="Settings" component={SettingScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </DataProvider>
+    );
+}
