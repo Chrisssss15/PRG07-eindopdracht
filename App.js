5333e1b10153664e06041e1822c278bc3f7f3ce7
Chris Tang <1073412@hr.nl>
2025-07-02 14:11:55 +0200

    EDIT naam aangepast, stond namelijk dubbel in

--- App.js
+++ App.js
@@ -9,28 +9,32 @@ import MapScreen from './screens/map';
 import SettingScreen from './screens/setting';
 import { DataProvider } from './components/data';
 
+import { ThemeProvider } from './components/themeContext';
+
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function ListStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="List" component={ListScreen} options={{ title: 'List' }} />
-            <Stack.Screen name="ListDetail" component={ListDetailScreen} options={{ title: 'Details' }} />
+            <Stack.Screen name="List" component={ListScreen} options={{ title: 'ListScreen' }} />
+            <Stack.Screen name="ListDetail" component={ListDetailScreen} options={{ title: 'ListDetailScreen' }} />
         </Stack.Navigator>
     );
 }
 
 export default function App() {
     return (
-        <DataProvider>
-            <NavigationContainer>
-                <Tab.Navigator initialRouteName="List">
-                    <Tab.Screen name="List" component={ListStack} options={{ headerShown: false }} />
-                    <Tab.Screen name="Map" component={MapScreen} />
-                    <Tab.Screen name="Settings" component={SettingScreen} />
-                </Tab.Navigator>
-            </NavigationContainer>
-        </DataProvider>
+        <ThemeProvider>
+            <DataProvider>
+                <NavigationContainer>
+                    <Tab.Navigator initialRouteName="Map">
+                        <Tab.Screen name="List" component={ListStack} options={{ headerShown: false }} />
+                        <Tab.Screen name="Map" component={MapScreen} />
+                        <Tab.Screen name="Settings" component={SettingScreen} />
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </DataProvider>
+        </ThemeProvider>
     );
 }
